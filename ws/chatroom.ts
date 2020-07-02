263f22217693b48be83ee18289e229cdb6212509
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-07-03 04:19:59 +0530

    done

--- ws/chatroom.ts
+++ ws/chatroom.ts
@@ -1,19 +1,44 @@
-import { WebSocket } from "https://deno.land/std/ws/mod.ts";
-import {v4} from 'https://deno.land/std/uuid/mod.ts'
+import {
+  WebSocket,
+  isWebSocketCloseEvent,
+} from "https://deno.land/std/ws/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
+let sockets = new Map<string, WebSocket>();
 
-let sockets = new Map<string,WebSocket>()
+interface BroadcastObj{
+    name:string,
+    mssg:string
+}
+
+///Broadcast events
+
+const broadcastEvent = (obj:BroadcastObj)=>{
+    sockets.forEach((ws:WebSocket)=>{
+        ws.send(JSON.stringify(obj))
+    })
+}
 
 const chatConenction = async (ws: WebSocket) => {
-  console.log("new socket connection");
-
-
   //add new ws connecton to map
-  const uid = v4.generate()
-  sockets.set(uid,ws)
+  const uid = v4.generate();
+  sockets.set(uid, ws);
 
-  console.log(sockets)
+  for await (const ev of ws) {
+    console.log(ev);
+
+    //delete socket if connn closed
+    if(isWebSocketCloseEvent(ev)){
+        sockets.delete(uid)
+    }
+
+    //create ev object if ev is string
+
+    if (typeof ev === "string") {
+      let evObj = JSON.parse(ev);
+      broadcastEvent(evObj)
+    }
+  }
 };
 
-
-export {chatConenction}
+export { chatConenction };
